8690a99484ea21dae195faf17aab0882a41f07b0
Fabrício Souza <fabricioiaronka1@gmail.com>
2026-04-28 15:49:00 -0300

    bug-fix: Changing path routes parameter

--- src/routes/path_routes.py
+++ src/routes/path_routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from use_cases.path_use_cases import PathUseCases
+from src.use_cases.path_use_cases import PathUseCases
 
 # Classe feita para retornar ao front a resposta(em formato de lista) do caminho escolhido
 
@@ -8,22 +8,16 @@ router = APIRouter(prefix="/rota", tags=["Caminhos"])
 path_use_case = PathUseCases()
 
 @router.get("/")
-def get_graph(response):
-    start = response.start
-    end = response.end
+def get_path(start:str, end:str):
 
     return path_use_case.find_path(start, end)
 
 @router.get("/kruskal")
-def get_graph(response):
-    start = response.start
-    end = response.end
+def get_kruskal_path(start:str, end:str):
 
     return path_use_case.find_path_kruskal(start, end)
 
 @router.get("/genetico")
-def get_graph(response):
-    start = response.start
-    end = response.end
+def get_genetic_path(start:str, end:str):
 
     return path_use_case.find_path_genetico(start, end)
